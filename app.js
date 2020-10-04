5ee5ead33512267c84c6b96dd80ad2912a427da2
SoorajGhosh <soorajghosh78@gmail.com>
2020-10-04 20:44:16 +0530

    Result Methods gives results now. NEXT: Daily Calorie intake update, body fat perc formula update, UX update for mobile users and finally updating the result values once calculate is pressed and then form values are changed.

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
 var dataCalculator = (function(){
-    // var bmi, bmr, maintCal, bodyFatPerc, leanBodyWeight, dailyCalIntake, proteinReq, carbReq, fatReq;
-    // var height, weight, age, activity, goal, gender;
-    // var invalidField;
-    
-    
-    // Check if all values are present
-    function formValuesCheck(obj){
-        for (var item in obj){
-            if (!obj[item]){
-                return item
-            }
-        }
-        return true
-    }
 
-    function checkUnits(hgtUnt,wgtUnt){
-        var hgtConst, wgtConst;
+    function getConstants(hgtUnt,wgtUnt, activity, goal, gender){
+        var hgtConst, wgtConst, activityConst, goalConst, genderBMRConst, genderBodyFatPerc;
 
         // Checking the Height Unit
         if (hgtUnt === 'cms'){
@@ -30,19 +16,75 @@ var dataCalculator = (function(){
         } else if (wgtUnt === 'lbs'){
             wgtConst = 2.2;
         }
-        return [hgtConst, wgtConst]
+
+        // Checking the Activity Unit
+        if (activity === 'no'){
+            activityConst = 1.2;
+        } else if (activity === 'little'){
+            activityConst = 1.35;
+        } else if (activity === 'moderate'){
+            activityConst = 1.5;
+        } else if (activity === 'intense'){
+            activityConst = 1.6;
+        } else if (activity === 'hard'){
+            activityConst = 1.75;
+        }
+
+        // Checking the Goal Unit
+        if (goal === 'maintain'){
+            goalConst = '=';
+        } else if (goal === 'gain'){
+            goalConst = '>';
+        } else if (goal === 'loss'){
+            goalConst = '<';
+        }
+
+        // Checking the Gender Unit
+        if (gender === 'male'){
+            genderBMRConst = 5;
+            genderBodyFatPerc = 16.2
+        } else if (gender === 'female'){
+            genderBMRConst = -161;
+            genderBodyFatPerc = 5.4
+        }
+
+        return [hgtConst, wgtConst, activityConst, goalConst, genderBMRConst, genderBodyFatPerc]
+    }
+
+    function getDoubleDecimal(val){
+        return (Math.round(val*100))/100;
+    }
+
+    function calculate(form_val){
+        [hgtVar, wgtVar, activityConst, goalConst, genderBMRConst, genderBodyFatPerc] = getConstants(form_val.heightUnit,form_val.weightUnit,
+                                        form_val.activity, form_val.goal, form_val.gender, 
+                                    );
+        var weight = (form_val.weight)/wgtVar;
+        var height = (form_val.height)/hgtVar;
+        var age = (form_val.age);
+        var bmi_val = Math.round(10000*weight/Math.pow(height,2));
+        var bmi_unit = 'unit';
+        var bmr_val = (10 * weight) + (6.25 * height) - (5 * age)  + genderBMRConst;
+        var bmr_unit = 'kcal/day';
+        var maintenance_cal_val = bmr_val*activityConst;
+        var maintenance_cal_unit = 'kcal/day';
+        var body_fat_perc_val = (1.20 * bmi_val) + (0.23 * form_val.age) - genderBodyFatPerc;
+        var body_fat_perc_unit = '%';
+        var lean_body_weight_val = weight-(body_fat_perc_val*weight/100);
+        var lean_body_weight_unit = form_val.weightUnit;
+        
+        return {
+            bmi : [getDoubleDecimal(bmi_val), bmi_unit],
+            bmr : [getDoubleDecimal(bmr_val), bmr_unit],
+            maintenance_cal : [getDoubleDecimal(maintenance_cal_val), maintenance_cal_unit],
+            body_fat_perc : [getDoubleDecimal(body_fat_perc_val), body_fat_perc_unit],
+            lean_body_weight : [getDoubleDecimal(lean_body_weight_val), lean_body_weight_unit],
+            daily_calorie : [goalConst+getDoubleDecimal(maintenance_cal_val), maintenance_cal_unit]
+        }
     }
 
     return {
-        calcFunction : function(formValues){
-            var weight = formValues.weight;
-            var height = formValues.height;
-            var hgtUnit = formValues.heightUnit;
-            var wgtUnit = formValues.weightUnit;
-            [hgtVar, wgtVar] = checkUnits(hgtUnit,wgtUnit);
-            var bmi = Math.round(10000*(weight/wgtVar)/Math.pow((height/hgtVar),2));
-            return [bmi,'unit']
-        },
+        resultObj : function(formValues){return calculate(formValues)},
     };
 })();
 
@@ -51,13 +93,6 @@ var dataCalculator = (function(){
 
 
 var UIcontroller = (function(){
-
-    /*
-    1. Get the variables from the form
-    2. Set the variable names and return them using an object
-    3. Update the Result
-    4. Update the Result Shown 
-    */
 
     var domValues = function(){
         return {
@@ -76,44 +111,39 @@ var UIcontroller = (function(){
         // RESULT METHODs
         resultMethods : document.querySelector('.result-methods'),
         allResultMethods : document.querySelectorAll('.result-method'),
-        bmi : document.querySelector('#bmi'),
+        // MACROS
+        allMacros : document.querySelectorAll('.macro'),
         // RESULT
         resultValue : document. querySelector('#calc-result-value'),
         resultUnit : document.querySelector('#calc-result-unit'),
         shownResult : document.getElementById('shown-result-category')
         }
     }
-    
-    // adding red focus on the invalid input class
-    var add_invalid_el = function(el){
-        invalid_el = domValues()[el+'Value'];
-        invalid_el.classList.add("form-red-input");
-        invalid_el.focus();
-    }
-
-    var showResultMethods = function(){
-        allResultMethods.style.display = 'block';
-    }
 
     return {
         dom : domValues(),
 
-        add_invalid_el : add_invalid_el,
+        // adding red focus on the invalid input class
+        add_invalid_el : function(el){
+            invalid_el = domValues()[el+'Value'];
+            invalid_el.classList.add("form-red-input");
+            invalid_el.focus();
+        },
 
+        // Updating and Displaying Results
         resultDisplay : function(valArr){
             this.dom.resultValue.innerHTML = valArr[0];
             this.dom.resultUnit.innerHTML = valArr[1];
         },
 
-        resultMethodsDisplay : function(displayType){
-            results = this.dom.allResultMethods;
+        // Changing Display type of elements
+        elementsDisplayChange : function(displayItems, displayType){
+            // results = this.dom.allResultMethods;
             function showResult(n) {
                 if(!n){n = 0;}
-                  
-                var showingVal = results[n].style.display = displayType;    
-              
-                if(n < results.length-1) {
-                  setTimeout(function() { showResult(n + 1); }, 500);
+                var showingVal = displayItems[n].style.display = displayType;
+                if(n < displayItems.length-1) {
+                  setTimeout(function() { showResult(n + 1); }, 1000);
                 }
             }
             setTimeout(showResult, 1000);
@@ -126,7 +156,7 @@ var UIcontroller = (function(){
 
 
 var controller = (function(dataCalc, UIctrl){
-    
+    var formValues, resultObj;
     /*
     1. Get the Field Input Data
     2. Add the data to the DataCalculator
@@ -138,8 +168,8 @@ var controller = (function(dataCalc, UIctrl){
 
     var main = function(){
 
-        // GETTING THE FORM VALUES
-        var formValues =  {
+        // SETTING AND GETTING THE FORM VALUES
+        formValues =  {
             height : dom.heightValue.value,
             heightUnit : dom.heightUnit.value,
             weight : dom.weightValue.value,
@@ -162,12 +192,9 @@ var controller = (function(dataCalc, UIctrl){
         }
 
         if (formValuesCheck(formValues)===true){
-            // DISPLAY THE RESULT METHODS
-            UIctrl.resultMethodsDisplay('inline-block');
-            // SENDING THE FORM VALUES TO THE DATA CONTROLLER AND RECIEVING THE RESULT OUTPUT ARRAY
-            var resultArr = dataCalc.calcFunction(formValues);
-            // SETTING THE RESULT IN THE UI
-            UIctrl.resultDisplay(resultArr);
+            UIctrl.elementsDisplayChange(dom.allResultMethods, 'inline-block');    // DISPLAY THE RESULT METHODS
+            resultObj = dataCalc.resultObj(formValues);     // SENDING THE FORM VALUES TO THE DATA CONTROLLER AND RECIEVING THE RESULT OUTPUT ARRAY
+            UIctrl.resultDisplay(resultObj.bmi);            // SETTING THE RESULT IN THE UI
         } else {
             UIctrl.add_invalid_el(formValuesCheck(formValues));
         }
@@ -175,7 +202,7 @@ var controller = (function(dataCalc, UIctrl){
     }
     
     var clearFunc = function(){
-        UIctrl.resultMethodsDisplay('none');    // Hiding the result methods
+        UIctrl.elementsDisplayChange(dom.allResultMethods,'none');    // Hiding the result methods
         dom.heightValue.value ='';              // Deleting values from form
         dom.heightUnit.value = 'cms';
         dom.weightValue.value = '';
@@ -184,25 +211,28 @@ var controller = (function(dataCalc, UIctrl){
         dom.activity.value = 'no';
         dom.goal.value = 'maintain';
         dom.gender.value = 'male';
-        dom.resultValue.innerHTML = 0;
+        dom.resultValue.innerHTML = 0;          // Setting default in results
         dom.resultUnit.innerHTML = 'unit';
         dom.shownResult.innerHTML = 'BMI'
     }
     
-    var checkEvent = function(event){
+    var checkResultType = function(event){
         var target_el = event.target;
         if (target_el.className === dom.allResultMethods[0].className){
-            target_el_id = target_el.id;
             dom.shownResult.innerHTML = target_el.innerHTML;
-            dom.shownResult.style.textTransform="uppercase"
+            dom.shownResult.style.textTransform="uppercase";
+            UIctrl.resultDisplay(resultObj[target_el.id]);
+            if (target_el.id === 'daily_calorie'){
+                UIctrl.elementsDisplayChange(dom.allMacros, 'block');
+            }
         }
     }
 
     dom.calcBtn.addEventListener('click', main);
     dom.clearBtn.addEventListener('click', clearFunc);
-    dom.resultMethods.addEventListener('click',checkEvent);
+    dom.resultMethods.addEventListener('click',checkResultType);
     
-    // THE CODE BELOW THE BUTTON CLICK ONLY HAAPENS AFTER THE BUTTON IS CLICKED
+    // THE CODE BELOW THE BUTTON CLICK ONLY HApPENS AFTER THE BUTTON IS CLICKED
 
     // Removing any form-red-input class
     (function removeInvalid(){
